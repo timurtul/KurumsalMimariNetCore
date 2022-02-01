0a8feeddab65787ef52243ad775de2ca18d2546c
Timur Tül <protimu72@gmail.com>
2022-02-01 22:20:57 +0300

    EntityDeğişiklikleri

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    //Class olması şart, IEntity'den türetilmiş olmalı yani Veritabanı nesnesi olması lazım.
+    //new -> Abstract ve Interface dışındaki classları yazabilirsiniz.
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        T Get(Expression<Func<T, bool>> filter = null);
+
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
